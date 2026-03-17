2af4f948c6dce92d7b2229ee3995e4c82df7f5c1
kjyotish <jyotishkumar236@gmail.com>
2026-03-17 22:00:22 +0530

    .

--- src/components/RecentProjectAdd.jsx
+++ src/components/RecentProjectAdd.jsx
@@ -8,22 +8,21 @@ function RecentProjectAdd() {
       title: "Song Finder AI Website",
       description:
         "Song Finder AI is a smart platform that helps video editors and creators quickly find the perfect songs for their videos based on mood, movement, language, and style. It analyzes your selected filters and suggests trending songs that match your vibe in seconds. Built with React, Next.js, Supabase, and the Gemini API, it offers a mobile-friendly UI and smooth user experience.",
-      image: "",
-      embed:
-        "", // optional embed link
+      image: "/songFinder.png",
+      embed: "", // optional embed link
       buttonText: "View Project",
       buttonLink: "https://www.songfinderai.com/",
     },
-    // {
-    //   title: "Website Landing Page UI",
-    //   description:
-    //     "In this project I have created  a modern ui design with illustrations, modern cards design, pricing cards, content hide/show button and latest design footer",
-    //   image: "",
-    //   embed:
-    //     "https://embed.figma.com/proto/NDUg66oD664sEY3Hg5lcyq/Abun-landing-page-UI?page-id=38%3A64&node-id=38-65&viewport=374%2C111%2C0.07&scaling=scale-down-width&content-scaling=fixed&embed-host=share", // optional embed link
-    //   buttonText: "View Project",
-    //   buttonLink: "https://www.behance.net/jyotishkumar46",
-    // },
+    {
+      title: "Website Landing Page UI",
+      description:
+        "In this project I have created  a modern ui design with illustrations, modern cards design, pricing cards, content hide/show button and latest design footer",
+      image: "",
+      embed:
+        "https://embed.figma.com/proto/NDUg66oD664sEY3Hg5lcyq/Abun-landing-page-UI?page-id=38%3A64&node-id=38-65&viewport=374%2C111%2C0.07&scaling=scale-down-width&content-scaling=fixed&embed-host=share", // optional embed link
+      buttonText: "View Project",
+      buttonLink: "https://www.behance.net/jyotishkumar46",
+    },
     {
       title: "Construction Website Frontend",
       description:
